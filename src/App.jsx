9d0e20ccfcc8ca1792a11d152dc3ca6c56d59509
Dylan Jara <dylan.jara@usach.cl>
2024-12-20 22:41:54 -0300

    add:file jsx (Mantenedor, Ranking, subirReceta, verReceta) updated:file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-
+//Modulos por hacer?
+//Home, Miperfil, subir_Receta, ver_Receta, Mantenedor, Ranking 
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import { Alan } from './pages/Alan'
 import { Miperfil } from './pages/Miperfil'
 import { New } from './pages/new'
+import { subirReceta } from './pages/subirReceta'
+import { verReceta } from './pages/verReceta'
+import { Mantenedor } from './pages/Mantenedor'
+import { Ranking } from './pages/Ranking'
 function App() {
   
 
@@ -14,6 +19,10 @@ function App() {
       <Route path="/alan" element={<Alan />} />
       <Route path="/miperfil" element={<Miperfil/>} />  
       <Route path="/new" element={<New/>} />
+      <Route path="/subirreceta" element={<subirReceta/>} />
+      <Route path="/verreceta" element={<verReceta/>} />
+      <Route path="/mantenedor" element={<Mantenedor/>} />
+      <Route path="/ranking" element={<Ranking/>} />
       <Route path="*" element={<h1>Not Found</h1>} />
 
 
---
+++ src/pages/Mantenedor.jsx
@@ -0,0 +1,10 @@
+import UserForm from "../componets/form"
+
+export const mant = () => {
+    return(
+        <div>
+            <UserForm/>
+            <h1 classname = "bg-red"></h1>
+        </div>
+    );
+}
