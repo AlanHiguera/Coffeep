1e2fedb603622895069ed04ee993dee36e5b0815
belenegri <belenegri9@gmail.com>
2024-12-22 20:09:45 -0300

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 
-import { Routes, Route, Router } from 'react-router-dom'
-import Home from './pages/Home'
-import { Alan } from './pages/Alan'
-import { Miperfil } from './pages/Miperfil'
-import { New } from './pages/new'
+import { Routes, Route} from 'react-router-dom'
 import LoginPage from './pages/Iniciosesion'
 import CreateRecipePage from './pages/CrearReceta'
 import Header from './componets/Lineasup'
@@ -18,10 +14,6 @@ function App() {
         <Header />
         <main className="flex-grow">
         <Routes> 
-          <Route path="/" element={<Home />} />
-          <Route path="/alan" element={<Alan />} />
-          <Route path="/miperfil" element={<Miperfil/>} />  
-          <Route path="/new" element={<New/>} />
           <Route path="/iniciosesion" element={<LoginPage/>} />
           <Route path="/crearreceta" element={<CreateRecipePage/>} />
           <Route path="*" element={<h1>Not Found</h1>} />
--- src/componets/contenidoHome.jsx
+++
@@ -1,9 +0,0 @@
-import './contenidoHome.css';
-export const ContenidoHome  = () => {
-    return (
-        <div>
-        <h1 className='hola'>ContenidoHome</h1>
-        <p>Este es el contenido de la pagina de inicio</p>
-        </div>
-    );
-    }
